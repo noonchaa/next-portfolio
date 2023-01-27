13275cb77edac7ffde64dc9dcc9703b21505eca0
noonchaa <enouchiha11@gmail.com>
2023-01-28 02:41:21 +0700

    about & contact page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,61 @@
-const About = () => {
+import Back from "@/components/back";
+import Layout from "@/components/layout";
+import Loading from "@/components/loading/loading";
+import { useEffect, useState } from "react";
+import { client } from "utils/client";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { useRouter } from "next/router";
+
+export const getStaticProps = async () => {
+    const about = await client.getEntries({ content_type: 'about', 'fields.seoTitle': 'Suyono' })
+    return {
+        props: {
+            data: about.items[0]
+        },
+        revalidate: 60
+    }
+}
+
+const About = ({ data }: props) => {
+    const [loading, setLoading] = useState(true);
+    const { seoTitle, seoDesc, about } = data.fields;
+    const router = useRouter();
+
+    useEffect(() => {
+        const timedLoad = setTimeout(() => {
+            if (document.readyState == 'complete') {
+                setLoading(false);
+            }
+        }, 1500)
+
+        return () => {
+            clearTimeout(timedLoad);
+        }
+    }, [])
+
+    if (!data) router.back();
+
     return (
-        <div className="flex justify-center items-center h-screen">
-            <p className="custom-font text-2xl">Comming Soon</p>
-        </div>
+        <Layout title={seoTitle} desc={seoDesc}>
+            <Loading loading={loading} />
+            <section className='container mx-auto relative px-2 py-12 lg:py-16'>
+                <Back />
+                <article className="prose">
+                    {documentToReactComponents(about)}
+                </article>
+            </section>
+        </Layout>
     )
+}
+
+type props = {
+    data: {
+        fields : {
+            seoTitle: string;
+            seoDesc: string;
+            about: any
+        }
+    }
 }
 
 export default About;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,70 @@
-const Contact = () => {
+import Back from "@/components/back"
+import Layout from "@/components/layout"
+import Loading from "@/components/loading/loading"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import { client } from "utils/client"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { INLINES } from '@contentful/rich-text-types'
+
+export const getStaticProps = async () => {
+    const about = await client.getEntries({ content_type: 'about', 'fields.seoTitle': 'Suyono' })
+    return {
+        props: {
+            data: about.items[0]
+        },
+        revalidate: 60
+    }
+}
+
+const Contact = ({ data }: props) => {
+    const [loading, setLoading] = useState(true);
+    const { seoTitle, seoDesc, contact } = data.fields;
+    const router = useRouter();
+    
+    const options = {
+        renderNode: {
+          [INLINES.HYPERLINK]: (node:any) => {
+            return <a href={node.data.uri} target='_blank'>{node.content[0].value}</a>;
+          }
+        }
+      }
+
+    useEffect(() => {
+        const timedLoad = setTimeout(() => {
+            if (document.readyState == 'complete') {
+                setLoading(false);
+            }
+        }, 1500)
+
+        return () => {
+            clearTimeout(timedLoad);
+        }
+    }, [])
+
+    if (!data) router.back();
+
     return (
-        <div className="flex justify-center items-center h-screen">
-            <p className="custom-font text-2xl">Comming Soon</p>
-        </div>
+        <Layout title={seoTitle} desc={seoDesc}>
+            <Loading loading={loading} />
+            <section className='container mx-auto relative px-2 py-12 lg:py-16'>
+                <Back />
+                <article className="prose">
+                    {documentToReactComponents(contact,options)}
+                </article>
+            </section>
+        </Layout>
     )
+}
+
+type props = {
+    data: {
+        fields : {
+            seoTitle: string;
+            seoDesc: string;
+            contact: any
+        }
+    }
 }
 
 export default Contact;
