396e657b0f8c00ac7f47e3314f6095dfba428403
noonchaa <enouchiha11@gmail.com>
2023-01-29 00:26:52 +0700

    fix back button on work page

--- src/components/back.tsx
+++ src/components/back.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 const Back = () => {
     const router = useRouter();
     return (
-        <div onClick={() => router.back()} className='cursor-pointer absolute top-0 right-2 bg-slate-900 text-white rounded-b-xl p-2 font-mono shadow'>
+        <div onClick={() => router.asPath == '/work' ? router.push('/') : router.back()} className='cursor-pointer absolute top-0 right-2 bg-slate-900 text-white rounded-b-xl p-2 font-mono shadow'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
             </svg>
