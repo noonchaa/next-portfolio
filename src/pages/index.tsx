9f29d8dfc79f38e4e23d02b3bde1a17d0cdb6cbb
noonchaa <enouchiha11@gmail.com>
2023-01-31 03:46:42 +0700

    add framer motion & change bg by day time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import Layout from '@/components/layout';
 import Loading from '@/components/loading/loading'
 import Link from 'next/link';
 import { useEffect, useState } from 'react'
+import { motion } from "framer-motion"
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const [clock, setClock] = useState('');
+  const time = new Date().getHours();
+  const curentDay = (time >= 0 && time <= 3) ? 'night' : (time >= 4 && time <= 10) ? 'morning' : (time >= 11 && time <= 16) ? 'day' : (time >= 17 && time <= 21) ? 'evening' : 'night';
 
   useEffect(() => {
     const timedLoad = setTimeout(() => {
@@ -29,10 +32,10 @@ export default function Home() {
   return (
     <Layout title='Suyono' desc='Saya seorang front-end developer. Jika anda tertarik membuat website seperti ini bisa langsung anda beli disini, atau anda menginginkan website sesuai keinginan anda bisa langsung hubungi saya'>
       <Loading loading={loading} />
-      <section className='container mx-auto relative'>
-        <div className='absolute top-0 right-2 bg-slate-900 text-white rounded-b-xl p-2 font-mono shadow'>
+      <section className='container mx-auto relative z-10'>
+        <motion.div whileHover={{scale:1.2}} className='absolute top-0 right-2 bg-slate-900 text-white rounded-b-xl p-2 font-mono shadow'>
           <p>{clock}</p>
-        </div>
+        </motion.div>
         <div className='flex justify-center items-center flex-col gap-8 h-screen custom-font text-5xl xl:text-7xl'>
           <Link href={'/work'}>
             <p className='-skew-y-12 hover:skew-y-0 hover:bg-black/5 hover:backdrop-filter hover:backdrop-blur-md hover:p-2 hover:scale-150 rounded'>Work</p>
@@ -48,6 +51,7 @@ export default function Home() {
           </Link>
         </div>
       </section>
+      <div className='absolute top-0 left-0 right-0 bottom-0 z-0 bg-cover bg-no-repeat bg-center' style={{backgroundImage:`url(${curentDay}.jpg)`}}></div>
     </Layout>
   )
 }
