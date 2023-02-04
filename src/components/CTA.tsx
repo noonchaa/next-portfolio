58ce9b17eed2dafc8a86dea3312d07567972e7b7
noonchaa <enouchiha11@gmail.com>
2023-02-04 20:30:29 +0700

    fix quotes

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -3,7 +3,7 @@ const CTA = () => {
         <div className="container grid grid-cols-1 gap-8 px-3 py-12 mx-auto lg:grid-cols-2 text-slate-900">
             <div className="text-center">
                 <p className="text-3xl font-semibold tracking-tight">Web Development</p>
-                <p className="mt-3">Pembuatan website adalah keahlian kami. Sebagai expert dalam bidang ini kami tidak "asal bikin website", tetapi kami juga peduli dengan cara memajukan usaha Anda</p>
+                <p className="mt-3">Pembuatan website adalah keahlian kami. Sebagai expert dalam bidang ini kami tidak &quot;asal bikin website&ldquo;, tetapi kami juga peduli dengan cara memajukan usaha Anda</p>
                 <a href="#" className="inline-flex items-center justify-center w-full px-5 py-2 mt-6 text-white transition-colors duration-300 bg-red-600 rounded-lg sm:w-auto">
                     Start now
                 </a>
